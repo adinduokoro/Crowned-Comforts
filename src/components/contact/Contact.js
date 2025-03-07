d2fbafded0749404143f12824f293820389a730b
Addie Okoro <adinduokoro95@gmail.com>
2025-03-06 19:21:23 -0500

    Working on the contact section.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import styles from "./Contact.module.css"
-import { Section } from '../../ux'
+import React from "react";
+import styles from "./Contact.module.css";
+import { Section } from "../../ux";
+import { contacts } from "./data";
 
 const Contact = () => {
   return (
-    <Section background="contactBackground">contact</Section>
-  )
-}
+    <Section background="contactBackground">
+      <div className={styles.contact}>
+        <ul className={styles.cards}>
+          {contacts.map((contact, index) => (
+            <li className={styles.card} key={index}>
+              <p className="button-regular">{contact.title}</p>
+              <p className="text-regular">
+                {contact.description}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
+};
 
-export default Contact
+export default Contact;
---
+++ src/components/contact/data.js
@@ -0,0 +1,5 @@
+export const contacts = [
+  { title: "Address", description: "1150 NW 72nd Ave. Tower I ste 455 #9843 Miami, Fl 33126" },
+  { title: "Email", description: "leasing@crownedcomforts.com" },
+  { title: "Phone", description: "+1 (410) 000-0000" },
+];
